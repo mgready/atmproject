e6718f9a909cf22b5f31ffdd403ecd68328860ed
mgready <79506353+mgready@users.noreply.github.com>
2021-02-25 09:22:31 +0500

    Update atm.java

--- ATM/src/atm/atm.java
+++ ATM/src/atm/atm.java
@@ -3,6 +3,6 @@ package atm;
 public class atm {
    public static void main (String[] args) {
 	   System.out.println("Hello world");
-	   System.out.println("Hello world");
+	   System.out.println("Hello world it's me");
    }
 }
